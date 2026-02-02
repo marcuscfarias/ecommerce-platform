4bd01435593c3a3cf584002a321e79107bbc9563
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-02-02 09:56:42 +0100

    test: add StringNotEmptyRuleTest

---
+++ src/Common/Ecommerce.Common.UnitTests/Domain/BusinessRules/StringNotEmptyRuleTests.cs
@@ -0,0 +1,65 @@
+﻿using Ecommerce.Common.Domain.BusinessRules.Rules;
+using Shouldly;
+
+namespace Ecommerce.Common.UnitTests.Domain.BusinessRules;
+
+public class StringNotEmptyRuleTests
+{
+    private const string FieldName = "FieldName";
+    private const string ErrorMessage = $"{FieldName} cannot be empty.";
+
+    [Fact]
+    public void IsMet_ValueHasContent_ReturnsTrue()
+    {
+        //arrange
+        var rule = new StringNotEmptyRule("valid", FieldName);
+
+        //act
+        var result = rule.IsMet();
+
+        //assert
+        result.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void IsMet_ValueIsNull_ReturnsFalseWithErrorMessage()
+    {
+        //arrange
+        var rule = new StringNotEmptyRule(null, FieldName);
+
+        //act
+        var result = rule.IsMet();
+
+        //assert
+        result.ShouldBeFalse();
+        rule.Error.ShouldBe(ErrorMessage);
+    }
+
+    [Fact]
+    public void IsMet_ValueIsEmpty_ReturnsFalseWithErrorMessage()
+    {
+        //arrange
+        var rule = new StringNotEmptyRule("", FieldName);
+
+        //act
+        var result = rule.IsMet();
+
+        //assert
+        result.ShouldBeFalse();
+        rule.Error.ShouldBe(ErrorMessage);
+    }
+
+    [Fact]
+    public void IsMet_ValueIsWhitespace_ReturnsFalseWithErrorMessage()
+    {
+        //arrange
+        var rule = new StringNotEmptyRule(" ", FieldName);
+
+        //act
+        var result = rule.IsMet();
+
+        //assert
+        result.ShouldBeFalse();
+        rule.Error.ShouldBe(ErrorMessage);
+    }
+}
