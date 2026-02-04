2eea921aee4d2e33a9309003b221e85207e6d600
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-02-04 08:25:27 +0100

    feat: add global exception handler structure

---
+++ src/Common/Ecommerce.Common.API/ErrorHandling/ErrorHandlingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Common.API.ErrorHandling;
+
+public static class ErrorHandlingExtensions
+{
+    public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder applicationBuilder)
+    {
+        applicationBuilder.UseExceptionHandler();
+
+        return applicationBuilder;
+    }
+
+    public static IServiceCollection AddExceptionHandling(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddProblemDetails();
+
+        return services;
+    }
+}
---
+++ src/Common/Ecommerce.Common.API/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Common.API.ErrorHandling;
+
+public sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
