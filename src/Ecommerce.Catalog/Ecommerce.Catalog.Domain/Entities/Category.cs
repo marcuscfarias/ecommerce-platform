82be5c9214fcf03d2a8fb379901122f41bdd0c7c
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-03-26 10:14:30 +0100

    feat(catalog): add Category entity

---
+++ src/Ecommerce.Catalog/Ecommerce.Catalog.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.Shared.Domain.Entities;
+
+namespace Ecommerce.Catalog.Domain.Entities;
+
+public sealed class Category(string name, string? description, bool isActive) : Entity
+{
+    public string Name { get; private set; } = name;
+    public string? Description { get; private set; } = description;
+    public bool IsActive { get; private set; } = isActive;
+}
---
+++ src/Ecommerce.Catalog/Ecommerce.Catalog.Domain/Repositories/ICatalogRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Ecommerce.Catalog.Domain.Repositories;
+
+public interface ICatalogRepository
+{
+    
+}
