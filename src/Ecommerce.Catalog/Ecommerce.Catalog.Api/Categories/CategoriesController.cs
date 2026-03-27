c35503bb1c43c975399c9cb52f3f67920c91f18c
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-03-27 08:30:27 +0100

    feat(catalog): add CategoriesController

---
+++ src/Ecommerce.Catalog/Ecommerce.Catalog.Api/Categories/CategoriesController.cs
@@ -0,0 +1,18 @@
+using Ecommerce.Catalog.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Catalog.Api.Categories;
+
+[ApiController]
+[Route("api/categories")]
+internal sealed class CategoriesController(ICatalogModule module) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(
+        [FromBody] CreateCategoryRequest request,
+        CancellationToken cancellationToken)
+    {
+        var id = await module.ExecuteCommandAsync(request.ToCommand(), cancellationToken);
+        return CreatedAtAction(nameof(CreateAsync), new { id }, null);
+    }
+}
