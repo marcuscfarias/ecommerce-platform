c7e16c1a964580c36aa2ee8bde5e179319e8c89b
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-04-14 10:44:26 +0200

    docs: change the scalar doc view

--- src/Ecommerce.Catalog/Ecommerce.Catalog.Api/Categories/UpdateCategory/UpdateCategoryRequest.cs
+++ src/Ecommerce.Catalog/Ecommerce.Catalog.Api/Categories/UpdateCategory/UpdateCategoryRequest.cs
@@ -2,7 +2,7 @@ using Ecommerce.Catalog.Application.Categories.UpdateCategory;
 
 namespace Ecommerce.Catalog.Api.Categories.UpdateCategory;
 
-public sealed record UpdateCategoryRequest(string Name, string Slug, bool IsActive, string? Description = null)
+public sealed record UpdateCategoryRequest(string Name, string Slug, bool IsActive, string? Description)
 {
     internal UpdateCategoryCommand ToCommand(int id) => new(id, Name, Slug, Description, IsActive);
 }
