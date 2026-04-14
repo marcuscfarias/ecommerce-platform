e92ed3a91bb5801e57163f4a9d34b873eb9ea5bb
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-04-14 11:22:05 +0200

    test: add ResourceNotFoundExceptionTests

--- src/Ecommerce.Shared/Ecommerce.Shared.Application/Exceptions/ResourceNotFoundException.cs
+++ src/Ecommerce.Shared/Ecommerce.Shared.Application/Exceptions/ResourceNotFoundException.cs
@@ -2,8 +2,8 @@ using Ecommerce.Shared.Domain.Exceptions;
 
 namespace Ecommerce.Shared.Application.Exceptions;
 
-public class ResourceNotFoundException(string resourceName, object id)
-    : Exception($"Resource {resourceName} with Id {id} couldn't be found."), IAppException
+public class ResourceNotFoundException(string resourceName, int id)
+    : Exception($"{resourceName} with Id {id} couldn't be found."), IAppException
 {
     public int StatusCode => 404;
 }
---
+++ src/Ecommerce.Shared/Ecommerce.Shared.UnitTests/Application/Exceptions/ResourceNotFoundExceptionTests.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.Shared.Application.Exceptions;
+
+namespace Ecommerce.Shared.UnitTests.Application.Exceptions;
+
+public class ResourceNotFoundExceptionTests
+{
+    [Fact]
+    public void Construct_StatusCode_ShouldReturn404()
+    {
+        //arrange 
+        var ex = new ResourceNotFoundException("Entity", 1);
+        
+        //act
+        
+        //assert
+        ex.StatusCode.ShouldBe(404);
+    }
+
+    [Fact]
+    public void Construct_MessageError_ShouldConcatenateMessage()
+    {
+        //arrange
+        string entity = "Entity";
+        int id = 1;
+        var ex = new ResourceNotFoundException(entity, id);
+        
+        //act
+        
+        //assert
+        ex.Message.ShouldBe($"{entity} with Id {id} couldn't be found.");
+    }
+}
