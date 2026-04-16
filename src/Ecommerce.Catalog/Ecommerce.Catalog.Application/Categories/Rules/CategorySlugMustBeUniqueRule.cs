2cb29c53facc8a2b4c57031a852e729d0a33f67d
Marcus Farias <marcusczupriniakifarias@gmail.com>
2026-04-16 17:16:11 +0200

    refactor(catalog): change CategorySlugMustBeUniqueRule visibility to internal

--- src/Ecommerce.Catalog/Ecommerce.Catalog.Application/Categories/Rules/CategorySlugMustBeUniqueRule.cs
+++ src/Ecommerce.Catalog/Ecommerce.Catalog.Application/Categories/Rules/CategorySlugMustBeUniqueRule.cs
@@ -2,7 +2,7 @@ using Ecommerce.Shared.Domain.BusinessRules;
 
 namespace Ecommerce.Catalog.Application.Categories.Rules;
 
-public class CategorySlugMustBeUniqueRule(bool exists) : IBusinessRule
+internal class CategorySlugMustBeUniqueRule(bool exists) : IBusinessRule
 {
     public bool IsMet() => exists is not true;
     public string ErrorMessage => "A category with this slug already exists.";
